47b84a1729f473bfe67bde20d07dcc98a160aff4
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-28 03:31:01 -0300

    Update names.asm
    +steel and dark types, remove bird

--- data/types/names.asm
+++ data/types/names.asm
@@ -7,21 +7,21 @@ TypeNames:
 	dw .Poison
 	dw .Ground
 	dw .Rock
-	dw .Bird
 	dw .Bug
-	dw .Ghost
+	dw .Steel
+	dw .Ice
+	dw .Dragon
 
 REPT UNUSED_TYPES_END - UNUSED_TYPES
 	dw .Normal
 ENDR
 
+	dw .Ghost
 	dw .Fire
 	dw .Water
 	dw .Grass
 	dw .Electric
 	dw .Psychic
-	dw .Ice
-	dw .Dragon
 
 	assert_table_length NUM_TYPES
 
@@ -29,15 +29,16 @@ ENDR
 .Fighting: db "FIGHTING@"
 .Flying:   db "FLYING@"
 .Poison:   db "POISON@"
+.Ground:   db "GROUND@"
+.Rock:     db "ROCK@"
+.Bug:      db "BUG@"
+.Ghost:    db "GHOST@"
 .Fire:     db "FIRE@"
 .Water:    db "WATER@"
 .Grass:    db "GRASS@"
 .Electric: db "ELECTRIC@"
 .Psychic:  db "PSYCHIC@"
 .Ice:      db "ICE@"
-.Ground:   db "GROUND@"
-.Rock:     db "ROCK@"
-.Bird:     db "BIRD@"
-.Bug:      db "BUG@"
-.Ghost:    db "GHOST@"
+.Steel:    db "STEEL@"
 .Dragon:   db "DRAGON@"
+.Dark:     db "DARK@"
