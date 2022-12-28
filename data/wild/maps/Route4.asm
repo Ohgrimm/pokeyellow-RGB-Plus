d01b1ac571cb6ad76d8380f8f6e2da85e90d22e7
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-28 18:04:14 -0500

    Update Route4.asm

--- data/wild/maps/Route4.asm
+++ data/wild/maps/Route4.asm
@@ -2,14 +2,14 @@ Route4WildMons:
 	def_grass_wildmons 20 ; encounter rate
 	db 12, RATTATA
 	db 12, SPEAROW
-	db 10, SANDSHREW
+	db 12, SANDSHREW
 	db 10, EKANS
 	db 12, EKANS
 	db 12, SANDSHREW
 	db 10, JIGGLYPUFF
-	db 10, JIGGLYPUFF
+	db 15, JIGGLYPUFF
 	db 10, PSYDUCK
-	db 10, PSYDUCK
+	db 15, PSYDUCK
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
