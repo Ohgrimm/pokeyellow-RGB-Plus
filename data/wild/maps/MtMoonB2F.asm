6c4f30de2b699d6b2201cb3470681b0e5e6e3df2
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-28 18:18:40 -0500

    Update MtMoonB2F.asm

--- data/wild/maps/MtMoonB2F.asm
+++ data/wild/maps/MtMoonB2F.asm
@@ -1,11 +1,11 @@
 MtMoonB2FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db 10, ZUBAT
 	db 10, GEODUDE
+	db 12, GEODUDE
+	db 10, ZUBAT
+	db 10, PARAS
 	db 10, PARAS
 	db 10, SANDSHREW
-	db 10, SANDSHREW
-	db 10, PARAS
 	db 10, CLEFAIRY
 	db 10, CLEFAIRY
 	db 12, CLEFAIRY
