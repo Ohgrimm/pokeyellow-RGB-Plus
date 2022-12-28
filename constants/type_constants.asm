0c4253ac2e06223022db492df8c3edd8da2feb25
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-28 03:37:19 -0300

    Update type_constants.asm
    New types

--- constants/type_constants.asm
+++ constants/type_constants.asm
@@ -8,21 +8,22 @@ DEF PHYSICAL EQU const_value
 	const POISON       ; $03
 	const GROUND       ; $04
 	const ROCK         ; $05
-	const BIRD         ; $06
-	const BUG          ; $07
-	const GHOST        ; $08
+	const BUG          ; $06
+	const STEEL        ; $07
+	const ICE.         ; $08
+	const DRAGON       ; $0
 
 DEF UNUSED_TYPES EQU const_value
 	const_next 20
 DEF UNUSED_TYPES_END EQU const_value
 
 DEF SPECIAL EQU const_value
-	const FIRE         ; $14
-	const WATER        ; $15
-	const GRASS        ; $16
-	const ELECTRIC     ; $17
-	const PSYCHIC_TYPE ; $18
-	const ICE          ; $19
-	const DRAGON       ; $1A
+	const GHOST        ; $14
+	const FIRE         ; $15
+	const WATER        ; $16
+	const GRASS        ; $17
+	const ELECTRIC     ; $18
+	const PSYCHIC_TYPE ; $19
+	const DARK         ; $1A
 
 DEF NUM_TYPES EQU const_value
