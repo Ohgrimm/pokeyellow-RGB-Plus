39895dc16f4208049cd5ba2d48bae1a2ac50d338
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-28 17:52:28 -0500

    Update Route3.asm

--- data/wild/maps/Route3.asm
+++ data/wild/maps/Route3.asm
@@ -1,15 +1,15 @@
 Route3WildMons:
 	def_grass_wildmons 20 ; encounter rate
-	db 10, RATTATA
-	db 10, SPEAROW
-	db 10, EKANS
+	db 8, RATTATA
+	db 8, SPEAROW
+	db 8, EKANS
+	db 8, SANDSHREW
 	db 10, SANDSHREW
-	db 12, SANDSHREW
-	db 12, EKANS
+	db 10, EKANS
+	db 8, MANKEY
 	db 10, MANKEY
-	db 12, MANKEY
 	db 10, JIGGLYPUFF
-	db 12, JIGGLYPUFF
+	db 10, JIGGLYPUFF
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
