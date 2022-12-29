c8ed31a107e7e1a6e447ebb04bea9743b0611076
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-28 23:12:11 -0300

    Update Route22.asm

--- data/wild/maps/Route22.asm
+++ data/wild/maps/Route22.asm
@@ -1,15 +1,15 @@
 Route22WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db  4, NIDORAN_F
-	db  4, NIDORAN_M
-	db  4, SPEAROW
-	db  4, MANKEY
-	db  4, MANKEY
-	db  5, SPEAROW
-	db  5, SPEAROW
-	db  5, DODUO
-	db  5, PONYTA
-	db  5, PONYTA
+	db  4, NIDORAN_F ; 20
+	db  4, NIDORAN_M ; 20
+	db  4, SPEAROW ; 15
+	db  4, MANKEY ; 10
+	db  4, SPEAROW ; 10
+	db  5, MANKEY ; 10
+	db  5, SPEAROW ; 5
+	db  5, DODUO ; 5
+	db  5, PONYTA ; 4
+	db  5, PONYTA ; 1
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
