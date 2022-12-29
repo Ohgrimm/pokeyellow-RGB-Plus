db8ac7665122d8a14d414d9f4faaee895b1b6a06
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-28 23:20:54 -0300

    Update MtMoon1F.asm

--- data/wild/maps/MtMoon1F.asm
+++ data/wild/maps/MtMoon1F.asm
@@ -1,15 +1,15 @@
 MtMoon1FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db  8, ZUBAT
-	db 10, ZUBAT
-	db  8, GEODUDE
-	db  8, SANDSHREW
-	db 10, SANDSHREW
-	db 10, GEODUDE
-	db  8, PARAS
-	db 10, PARAS
-	db 10, CLEFAIRY
-	db 10, CLEFAIRY
+	db  8, ZUBAT ; 20
+	db 10, ZUBAT ; 20
+	db  8, GEODUDE ; 15
+	db  8, ZUBAT ; 10
+	db 10, SANDSHREW ; 10
+	db 10, SANDSHREW ; 10
+	db 10, GEODUDE ; 5
+	db 10, PARAS ; 5
+	db 10, CLEFAIRY ; 4
+	db 10, CLEFAIRY ; 1
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
