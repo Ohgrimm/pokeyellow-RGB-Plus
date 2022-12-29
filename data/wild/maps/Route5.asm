8d25dc5339c2759cf805bf5e8fd003cb0d81c499
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-28 23:39:21 -0300

    Update Route5.asm

--- data/wild/maps/Route5.asm
+++ data/wild/maps/Route5.asm
@@ -1,11 +1,11 @@
 Route5WildMons:
 	def_grass_wildmons 15 ; encounter rate
 	db 15, MEOWTH ; 20
-	db 15, JIGGLYPUFF ; 20
-	db 15, BELLSPROUT ; 15
-	db 15, ODDISH ; 10
-	db 15, ODDISH ; 10
+	db 16, PIDGEY ; 20
+	db 15, JIGGLYPUFF ; 15
 	db 15, BELLSPROUT ; 10
+	db 15, ODDISH ; 10
+	db 15, ABRA ; 10
 	db 18, PIDGEOTTO ; 5
 	db 15, VULPIX ; 5
 	db 15, GROWLITHE ; 4
