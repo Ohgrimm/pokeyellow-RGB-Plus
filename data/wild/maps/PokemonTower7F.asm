40a80fe4ca0f5042c1c40d479d431547441535fb
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-29 02:25:52 -0300

    Update PokemonTower7F.asm

--- data/wild/maps/PokemonTower7F.asm
+++ data/wild/maps/PokemonTower7F.asm
@@ -1,15 +1,15 @@
 PokemonTower7FWildMons:
-	def_grass_wildmons 20 ; encounter rate
-	db 24, GASTLY
-	db 25, GASTLY
-	db 26, GASTLY
-	db 27, GASTLY
-	db 28, GASTLY
-	db 23, GASTLY
-	db 24, CUBONE
-	db 29, GASTLY
-	db 24, HAUNTER
-	db 29, HAUNTER
+	def_grass_wildmons 10 ; encounter rate
+	db 24, GASTLY ; 20
+	db 24, GASTLY ; 20
+	db 25, HAUNTER ; 15
+	db 25, CUBONE ; 10
+	db 25, CUBONE ; 10
+	db 25, CUBONE ; 10
+	db 27, HAUNTER ; 5
+	db 29, HAUNTER ; 5
+	db 30, MAROWAK ; 4
+	db 20, MAROWAK ; 1
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
