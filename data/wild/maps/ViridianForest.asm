c3198d2e3d18c0da1148885474e4f2cb6d03fbae
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-28 23:17:56 -0300

    Update ViridianForest.asm

--- data/wild/maps/ViridianForest.asm
+++ data/wild/maps/ViridianForest.asm
@@ -1,15 +1,15 @@
 ViridianForestWildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db  5, CATERPIE
-	db  5, WEEDLE
-	db  5, PIDGEY
-	db  7, METAPOD
-	db  7, KAKUNA
-	db  7, PIDGEY
-	db  9, KAKUNA
-	db  9, METAPOD
-	db  5, BULBASAUR
-	db  5, BULBASAUR
+	db  5, CATERPIE ; 20
+	db  5, WEEDLE ; 20
+	db  9, PIDGEY ; 15
+	db  7, METAPOD ; 10
+	db  7, KAKUNA ; 10
+	db  6, CATERPIE ; 10
+	db  6, WEEDLE ; 5
+	db  6, WEEDLE ; 5
+	db 18, PIDGEOTTO ; 4
+	db  5, BULBASAUR ; 1
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
