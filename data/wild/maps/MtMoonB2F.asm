c8a232b548935a63273ab355628a038444034036
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-28 23:26:07 -0300

    Update MtMoonB2F.asm

--- data/wild/maps/MtMoonB2F.asm
+++ data/wild/maps/MtMoonB2F.asm
@@ -1,15 +1,15 @@
 MtMoonB2FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db 10, GEODUDE
-	db 12, SANDSHREW
-	db 10, ZUBAT
-	db 10, PARAS
-	db 10, PARAS
-	db 10, ONIX
-	db 10, CLEFAIRY
-	db 10, CLEFAIRY
-	db 12, CLEFAIRY
-	db 12, CLEFAIRY
+	db 10, GEODUDE ; 20
+	db 12, GEODUDE ; 20
+	db 10, SANDSHREW ; 15
+	db 10, SANDSHREW ; 10
+	db 10, CLEFAIRY ; 10
+	db 10, PARAS ; 10
+	db 15, ZUBAT ; 5
+	db 15, ONIX ; 5
+	db 15, CLEFAIRY ; 4
+	db 15, CLEFAIRY ; 1
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
