23ae30f572cd08f07227d6cf3854a4706f7d1968
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-28 23:07:02 -0300

    Update Route1.asm

--- data/wild/maps/Route1.asm
+++ data/wild/maps/Route1.asm
@@ -1,15 +1,15 @@
 Route1WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db  3, PIDGEY
-	db  3, RATTATA
-	db  4, PIDGEY
-	db  4, RATTATA
-	db  4, RATTATA
-	db  4, PIDGEY
-	db  4, RATTATA
-	db  5, BELLSPROUT
-	db  5, ODDISH
-	db  5, ODDISH
+	db  3, PIDGEY ; 20
+	db  3, RATTATA ; 20
+	db  4, PIDGEY ; 15
+	db  4, RATTATA ; 10
+	db  4, PIDGEY ; 10
+	db  5, RATTATA ; 10
+	db  5, PIDGEY ; 5
+	db  5, BELLSPROUT ; 5
+	db  5, ODDISH ; 4
+	db  5, ODDISH ; 1
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
