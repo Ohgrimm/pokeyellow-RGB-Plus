005f206e4d92ff88d3e02f5240fd18635065bf05
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-29 01:38:55 -0300

    Update Route8.asm

--- data/wild/maps/Route8.asm
+++ data/wild/maps/Route8.asm
@@ -1,15 +1,15 @@
 Route8WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 20, PIDGEY
-	db 22, PIDGEY
-	db 20, RATTATA
-	db 15, ABRA
-	db 19, ABRA
-	db 24, PIDGEOTTO
-	db 19, JIGGLYPUFF
-	db 24, JIGGLYPUFF
-	db 20, KADABRA
-	db 27, KADABRA
+	db 20, PIDGEOTTO ; 20
+	db 20, RATICATE ; 20
+	db 20, ABRA ; 15
+	db 15, JIGGLYPUFF ; 10
+	db 20, MANKEY ; 10
+	db 20, KADABRA ; 10
+	db 24, KADABRA ; 5
+	db 20, GROWLITHE ; 5
+	db 20, GROWLITHE ; 4
+	db 20, GROWLITHE ; 1
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
