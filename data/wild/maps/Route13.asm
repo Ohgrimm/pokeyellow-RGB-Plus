e341ab2ce861071646056ce28aec23518d6746e5
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-29 11:35:51 -0500

    Update Route13.asm

--- data/wild/maps/Route13.asm
+++ data/wild/maps/Route13.asm
@@ -2,12 +2,12 @@ Route13WildMons:
 	def_grass_wildmons 15 ; encounter rate
 	db 30, GLOOM ; 20
 	db 30, WEEPINBELL ; 20
-	db 30, FARFETCHD ; 15
-	db 25, KRABBY ; 10
+	db 25, FARFETCHD ; 15
+	db 30, FARFETCHD ; 10
 	db 32, GLOOM ; 10
 	db 32, WEEPINBELL ; 10
-	db 34, GLOOM ; 5
-	db 34, WEEPINBELL ; 5
+	db 25, KRABBY ; 5
+	db 30, DITTO ; 5
 	db 35, FARFETCHD ; 4
 	db 35, FARFETCHD ; 1
 	end_grass_wildmons
