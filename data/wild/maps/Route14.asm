ab5e6c26f5e54770672747000b59b9b28b8fc928
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-29 11:37:43 -0500

    Update Route14.asm

--- data/wild/maps/Route14.asm
+++ data/wild/maps/Route14.asm
@@ -3,10 +3,10 @@ Route14WildMons:
 	db 25, VENONAT ; 20
 	db 30, VENONAT ; 20
 	db 30, FARFETCHD ; 15
-	db 35, FARFETCHD ; 10
+	db 25, KRABBY ; 10
 	db 34, GLOOM ; 10
 	db 34, WEEPINBELL ; 10
-	db 30, KRABBY ; 5
+	db 30, KINGLER ; 5
 	db 30, DITTO ; 5
 	db 35, VENOMOTH ; 4
 	db 35, VENOMOTH ; 1
