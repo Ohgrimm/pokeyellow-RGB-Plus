7017f938cdebc85025eed9b8682bd5a20d2c1a3c
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-29 01:50:01 -0300

    Update Route9.asm

--- data/wild/maps/Route9.asm
+++ data/wild/maps/Route9.asm
@@ -1,15 +1,15 @@
 Route9WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 16, NIDORAN_M
-	db 16, NIDORAN_F
-	db 18, RATTATA
-	db 18, NIDORAN_M
-	db 18, NIDORAN_F
-	db 17, SPEAROW
-	db 18, NIDORINO
-	db 18, NIDORINA
-	db 20, RATICATE
-	db 19, FEAROW
+	db 18, NIDORINO ; 20
+	db 18, NIDORINA ; 20
+	db 20, RATICATE ; 15
+	db 20, FEAROW ; 10
+	db 20, ARBOK ; 10
+	db 20, FEAROW ; 10
+	db 20, RATICATE ; 5
+	db 20, PRIMEAPE ; 5
+	db 22, SANDSLASH ; 4
+	db 22, SANDSLASH ; 1
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
