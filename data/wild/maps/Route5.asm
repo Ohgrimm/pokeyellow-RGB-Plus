ed244cf1c8cca24e60e6ef8d10f076fc7c9c4ef6
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-29 01:30:38 -0300

    Update Route5.asm

--- data/wild/maps/Route5.asm
+++ data/wild/maps/Route5.asm
@@ -3,8 +3,8 @@ Route5WildMons:
 	db 15, MEOWTH ; 20
 	db 16, PIDGEY ; 20
 	db 15, JIGGLYPUFF ; 15
-	db 15, BELLSPROUT ; 10
-	db 15, ODDISH ; 10
+	db 15, MANKEY ; 10
+	db 15, MANKEY ; 10
 	db 10, ABRA ; 10
 	db 18, PIDGEOTTO ; 5
 	db 15, GROWLITHE ; 5
