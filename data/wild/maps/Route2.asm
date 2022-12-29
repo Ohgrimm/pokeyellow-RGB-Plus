4b36be44eb0bc7cb0b058f6b27d0dbb80e5daf5a
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-28 23:10:16 -0300

    Update Route2.asm

--- data/wild/maps/Route2.asm
+++ data/wild/maps/Route2.asm
@@ -1,15 +1,15 @@
 Route2WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db  5, RATTATA
-	db  5, PIDGEY
-	db  5, NIDORAN_M
-	db  5, NIDORAN_F
-	db  6, NIDORAN_F
-	db  6, NIDORAN_M
-	db  6, PIDGEY
-	db  6, CATERPIE
-	db  6, WEEDLE
-	db  6, WEEDLE
+	db  5, RATTATA ; 20
+	db  5, PIDGEY ; 20
+	db  5, NIDORAN_M ; 15
+	db  5, NIDORAN_F ; 10
+	db  5, NIDORAN_F ; 10
+	db  6, RATTATA ; 10
+	db  5, NIDORAN_M ; 5
+	db  5, CATERPIE ; 5
+	db  5, WEEDLE ; 4
+	db  5, WEEDLE ; 1
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
