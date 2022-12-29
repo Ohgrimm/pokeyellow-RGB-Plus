880bff1266c1cc4308de5fa79e6b00888bfa9adb
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-29 01:25:03 -0300

    Update Route7.asm

--- data/wild/maps/Route7.asm
+++ data/wild/maps/Route7.asm
@@ -1,15 +1,15 @@
 Route7WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 20, PIDGEOTTO ; 20
+	db 22, PIDGEOTTO ; 20
 	db 20, RATICATE ; 20
-	db 20, RATTATA ; 15
-	db 15, ABRA ; 10
-	db 19, ABRA ; 10
-	db 24, PIDGEOTTO ; 10
-	db 26, ABRA ; 5
-	db 20, GROWLITHE ; 5
-	db 20, GROWLITHE ; 4
-	db 20, GROWLITHE ; 1
+	db 15, ABRA ; 15
+	db 20, WEEPINBELL ; 10
+	db 20, GLOOM ; 10
+	db 20, MEOWTH ; 10
+	db 22, MEOWTH ; 5
+	db 20, VULPIX ; 5
+	db 20, VULPIX ; 4
+	db 20, VULPIX ; 1
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
