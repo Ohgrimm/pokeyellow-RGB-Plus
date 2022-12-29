30b1c9a8b24952ddd88a6f346647673e631e2973
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-29 10:55:05 -0500

    Update Route11.asm

--- data/wild/maps/Route11.asm
+++ data/wild/maps/Route11.asm
@@ -1,16 +1,18 @@
 Route11WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 16, PIDGEY
-	db 15, RATTATA
-	db 18, PIDGEY
-	db 15, DROWZEE
-	db 17, RATTATA
-	db 17, DROWZEE
-	db 18, PIDGEOTTO
-	db 20, PIDGEOTTO
-	db 19, DROWZEE
-	db 17, RATICATE
+	db 16, SPEAROW ; 20
+	db 15, RATTATA ; 20
+	db 18, SPEAROW ; 15
+	db 15, DROWZEE ; 10
+	db 17, RATTATA ; 10
+	db 17, DROWZEE ; 10
+	db 18, SPEAROW ; 5
+	db 20, SPEAROW ; 5
+	db 19, DROWZEE ; 4
+	db 17, RATICATE ; 1
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
+
+SPEAROW / PIDGEY, RATTATA, DROWZEE, EKANS, SANDSHREW
