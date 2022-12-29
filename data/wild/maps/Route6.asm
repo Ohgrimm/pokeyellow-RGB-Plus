df9951be69c85c4a0c55a659fb473da081899385
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-28 23:46:00 -0300

    Update Route6.asm

--- data/wild/maps/Route6.asm
+++ data/wild/maps/Route6.asm
@@ -1,26 +1,26 @@
 Route6WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 15, PIDGEY
-	db 14, RATTATA
-	db  7, ABRA
-	db 16, PIDGEY
-	db 16, RATTATA
-	db 17, PIDGEY
-	db 17, PIDGEOTTO
-	db  3, JIGGLYPUFF
-	db  5, JIGGLYPUFF
-	db  7, JIGGLYPUFF
+	db 15, PIDGEY ; 20
+	db 14, RATTATA ; 20
+	db  7, ABRA ; 15
+	db 16, PIDGEY ; 10
+	db 16, RATTATA ; 10
+	db 17, PIDGEY ; 10
+	db 17, PIDGEOTTO ; 5
+	db  3, JIGGLYPUFF ; 5
+	db  5, JIGGLYPUFF ; 4
+	db  7, JIGGLYPUFF ; 1
 	end_grass_wildmons
 
 	def_water_wildmons 3 ; encounter rate
-	db 15, PSYDUCK
-	db 15, PSYDUCK
-	db 15, PSYDUCK
-	db 15, PSYDUCK
-	db 15, PSYDUCK
-	db 15, PSYDUCK
-	db 15, PSYDUCK
-	db 15, PSYDUCK
-	db 15, GOLDUCK
-	db 20, GOLDUCK
+	db 15, PSYDUCK ; 20
+	db 15, PSYDUCK ; 20
+	db 15, PSYDUCK ; 15
+	db 15, PSYDUCK ; 10
+	db 15, PSYDUCK ; 10
+	db 15, PSYDUCK ; 10
+	db 15, PSYDUCK ; 5
+	db 15, PSYDUCK ; 5
+	db 15, GOLDUCK ; 4
+	db 20, GOLDUCK ; 1
 	end_water_wildmons
