1ea52a2dd4d557b4f5d88c6c7f3887370bb63cb4
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-29 02:20:26 -0300

    Update PokemonTower4F.asm

--- data/wild/maps/PokemonTower4F.asm
+++ data/wild/maps/PokemonTower4F.asm
@@ -1,14 +1,15 @@
 PokemonTower4FWildMons:
-	db 18, GASTLY ; 20
-	db 18, GASTLY ; 20
-	db 20, GASTLY ; 15
-	db 22, GASTLY ; 10
-	db 22, GASTLY ; 10
-	db 22, GASTLY ; 10
-	db 24, GASTLY ; 5
-	db 24, GASTLY ; 5
-	db 25, HAUNTER ; 4
-	db 25, HAUNTER ; 1
+def_grass_wildmons 10 ; encounter rate
+	db 20, GASTLY ; 20
+	db 20, GASTLY ; 20
+	db 22, GASTLY ; 15
+	db 24, GASTLY ; 10
+	db 24, GASTLY ; 10
+	db 24, GASTLY ; 10
+	db 25, HAUNTER ; 5
+	db 25, HAUNTER ; 5
+	db 27, HAUNTER ; 4
+	db 27, HAUNTER ; 1
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
