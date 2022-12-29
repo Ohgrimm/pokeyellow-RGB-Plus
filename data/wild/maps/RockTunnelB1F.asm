87107936122bf634bf62db7119599b6d94732141
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-29 02:38:56 -0300

    Update RockTunnelB1F.asm

--- data/wild/maps/RockTunnelB1F.asm
+++ data/wild/maps/RockTunnelB1F.asm
@@ -1,11 +1,11 @@
 RockTunnelB1FWildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 20, ZUBAT ; 20
-	db 20, GEODUDE ; 20
+	db 22, ZUBAT ; 20
+	db 22, GEODUDE ; 20
 	db 24, MACHOP ; 15
 	db 26, MACHOP ; 10
-	db 22, GOLBAT ; 10
-	db 25, GRAVELER ; 10
+	db 24, GOLBAT ; 10
+	db 27, GRAVELER ; 10
 	db 20, ONIX ; 5
 	db 25, ONIX ; 5
 	db 15, CHARMANDER ; 4
