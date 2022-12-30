f4fe4a87d23c7ddc3ea5f2dd367a9752b7f5a733
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-29 19:12:05 -0500

    Update Route15.asm

--- data/wild/maps/Route15.asm
+++ data/wild/maps/Route15.asm
@@ -1,11 +1,11 @@
 Route15WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 26, ODDISH ; 20
-	db 26, BELLSPROUT ; 20
+	db 26, VENONAT ; 20
+	db 26, FARFETCHD ; 20
 	db 24, VENONAT ; 15
-	db 32, PIDGEOTTO ; 10
-	db 28, ODDISH ; 10
-	db 28, BELLSPROUT ; 10
+	db 35, FARFETCHD ; 10
+	db 30, GLOOM ; 10
+	db 30, VICTREEBELL ; 10
 	db 30, LICKITUNG ; 5
 	db 30, TAURUS ; 5
 	db 36, VENOMOTH ; 4
