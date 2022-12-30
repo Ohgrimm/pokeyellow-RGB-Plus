ff5247125fcbab534a00bf6916740a9936fdc154
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-29 22:21:35 -0300

    Update CeruleanCaveB1F.asm

--- data/wild/maps/CeruleanCaveB1F.asm
+++ data/wild/maps/CeruleanCaveB1F.asm
@@ -2,11 +2,11 @@ CeruleanCaveB1FWildMons:
 	def_grass_wildmons 25 ; encounter rate
 	db 54, GOLBAT
 	db 59, GOLBAT
-	db 55, GRAVELER
-	db 52, RHYHORN
+	db 55, GOLBAT
+	db 52, RHYDON
 	db 62, RHYDON
-	db 60, DITTO
-	db 56, CHANSEY
+	db 60, RHYDON
+	db 56, RAICHU
 	db 65, DITTO
 	db 55, LICKITUNG
 	db 50, LICKITUNG
