d44d9d0537ec5dd38558aa6d9a328026f17da62c
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-29 22:13:55 -0300

    Update Route16.asm

--- data/wild/maps/Route16.asm
+++ data/wild/maps/Route16.asm
@@ -1,15 +1,15 @@
 Route16WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db 22, SPEAROW
-	db 22, DODUO
-	db 23, RATTATA
-	db 24, DODUO
-	db 24, RATTATA
-	db 26, DODUO
-	db 23, SPEAROW
-	db 24, FEAROW
-	db 25, RATICATE
-	db 26, RATICATE
+	db 22, SPEAROW ; 20
+	db 22, DODUO ; 20
+	db 23, RATTATA ; 15
+	db 24, DODUO ; 10
+	db 24, RATTATA ; 10
+	db 26, DODUO ; 10
+	db 23, SPEAROW ; 5
+	db 24, FEAROW ; 5
+	db 5, RATICATE ; 4
+	db 5, RATICATE ; 1
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
