1d4d7c73696b7fc0661af6ef6563fe63223f8adf
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-30 17:37:47 -0300

    Update VictoryRoad1F.asm

--- data/wild/maps/VictoryRoad1F.asm
+++ data/wild/maps/VictoryRoad1F.asm
@@ -1,15 +1,15 @@
 VictoryRoad1FWildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 26, GEODUDE
-	db 31, GEODUDE
-	db 36, GEODUDE
-	db 39, ZUBAT
-	db 44, ZUBAT
-	db 41, GEODUDE
-	db 43, ONIX
-	db 45, ONIX
-	db 41, GRAVELER
-	db 47, GRAVELER
+	db 35, GRAVELER
+	db 30, GRAVELER
+	db 35, ONIX
+	db 40, GOOBAT
+	db 35, GOLBAT
+	db 35, GRAVELER
+	db 40, ONIX
+	db 5, HITMONCHAN
+	db 5, HITMONLEE
+	db 5, HITMONLEE
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
