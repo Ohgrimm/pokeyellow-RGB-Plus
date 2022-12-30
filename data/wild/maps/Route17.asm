7ff5b6948f4b333573188c8cd7db46f6ebb8384a
Arthur Ferreira <arthur_f@hotmail.com>
2022-12-29 19:22:06 -0500

    Update Route17.asm

--- data/wild/maps/Route17.asm
+++ data/wild/maps/Route17.asm
@@ -1,15 +1,15 @@
 Route17WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db 25, DODUO
-	db 25, DODUO
-	db 25, DODUO
-	db 25, PONYTA
-	db 30, PONYTA
-	db 35, PONYTA
-	db 20, DODUO
-	db 30, DODUO
-	db 35, DODRIO
-	db 35, DODRIO
+	db 25, DODUO ; 20
+	db 25, DODUO ; 20
+	db 25, DODUO ; 15
+	db 25, PONYTA ; 10
+	db 30, PONYTA ; 10
+	db 35, PONYTA ; 10
+	db 20, DODUO ; 5
+	db 30, DODUO ; 5
+	db 35, DODRIO ; 4
+	db 35, DODRIO ; 1
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
