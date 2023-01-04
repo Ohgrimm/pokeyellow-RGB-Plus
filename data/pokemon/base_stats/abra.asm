58ba3cd042c96e0c95c98dfae83327a93d9e97c2
Arthur Ferreira <arthur_f@hotmail.com>
2023-01-04 00:01:01 -0300

    Update abra.asm

--- data/pokemon/base_stats/abra.asm
+++ data/pokemon/base_stats/abra.asm
@@ -1,11 +1,11 @@
 	db DEX_ABRA ; pokedex id
 
-	db  25,  20,  15,  90, 105
+	db  30,  15,  15,  90, 80
 	;   hp  atk  def  spd  spc
 
 	db PSYCHIC_TYPE, PSYCHIC_TYPE ; type
-	db 200 ; catch rate
-	db 73 ; base exp
+	db 195 ; catch rate
+	db 60 ; base exp
 
 	INCBIN "gfx/pokemon/front/abra.pic", 0, 1 ; sprite dimensions
 	dw AbraPicFront, AbraPicBack
