14770fab6571c5367ce57effc113c9b72c7439a6
Arthur Ferreira <arthur_f@hotmail.com>
2023-01-03 23:52:53 -0300

    Update alakazam.asm

--- data/pokemon/base_stats/alakazam.asm
+++ data/pokemon/base_stats/alakazam.asm
@@ -1,11 +1,11 @@
 	db DEX_ALAKAZAM ; pokedex id
 
-	db  55,  50,  45, 120, 135
+	db  60,  45,  45, 120, 135
 	;   hp  atk  def  spd  spc
 
 	db PSYCHIC_TYPE, PSYCHIC_TYPE ; type
-	db 50 ; catch rate
-	db 186 ; base exp
+	db 45 ; catch rate
+	db 210 ; base exp
 
 	INCBIN "gfx/pokemon/front/alakazam.pic", 0, 1 ; sprite dimensions
 	dw AlakazamPicFront, AlakazamPicBack
