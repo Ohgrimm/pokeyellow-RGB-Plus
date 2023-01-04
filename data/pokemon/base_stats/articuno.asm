e9fb82e589617b4799535f43a2f2c4bbe4ce81b0
Arthur Ferreira <arthur_f@hotmail.com>
2023-01-04 00:22:18 -0300

    Update articuno.asm

--- data/pokemon/base_stats/articuno.asm
+++ data/pokemon/base_stats/articuno.asm
@@ -1,11 +1,11 @@
 	db DEX_ARTICUNO ; pokedex id
 
-	db  90,  85, 100,  85, 125
+	db  90,  90, 105,  105, 120 ; (510) 0, +5, +5, +20, -5
 	;   hp  atk  def  spd  spc
 
 	db ICE, FLYING ; type
-	db 3 ; catch rate
-	db 215 ; base exp
+	db 5 ; catch rate
+	db 250 ; base exp
 
 	INCBIN "gfx/pokemon/front/articuno.pic", 0, 1 ; sprite dimensions
 	dw ArticunoPicFront, ArticunoPicBack
