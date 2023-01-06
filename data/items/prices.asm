48df2265a9b11bf2c8086da06ce25634864703dc
Arthur Ferreira <arthur_f@hotmail.com>
2023-01-06 14:46:06 -0500

    Update prices.asm

--- data/items/prices.asm
+++ data/items/prices.asm
@@ -7,9 +7,9 @@ ItemPrices::
 	bcd3 0     ; TOWN_MAP
 	bcd3 0     ; BICYCLE
 	bcd3 0     ; SURFBOARD
-	bcd3 100   ; SAFARI_BALL
+	bcd3 1000  ; SAFARI_BALL
 	bcd3 0     ; POKEDEX
-	bcd3 0     ; MOON_STONE
+	bcd3 5000  ; MOON_STONE
 	bcd3 100   ; ANTIDOTE
 	bcd3 100   ; BURN_HEAL
 	bcd3 100   ; ICE_HEAL
@@ -17,9 +17,9 @@ ItemPrices::
 	bcd3 100   ; PARLYZ_HEAL
 	bcd3 6000  ; FULL_RESTORE
 	bcd3 4500  ; MAX_POTION
-	bcd3 3000  ; HYPER_POTION
-	bcd3 600   ; SUPER_POTION
-	bcd3 300   ; POTION
+	bcd3 1500  ; HYPER_POTION
+	bcd3 500   ; SUPER_POTION
+	bcd3 250   ; POTION
 	bcd3 0     ; BOULDERBADGE
 	bcd3 0     ; CASCADEBADGE
 	bcd3 0     ; THUNDERBADGE
@@ -31,9 +31,9 @@ ItemPrices::
 	bcd3 100   ; ESCAPE_ROPE
 	bcd3 100   ; REPEL
 	bcd3 10000 ; OLD_AMBER
-	bcd3 2500  ; FIRE_STONE
-	bcd3 2500  ; THUNDER_STONE
-	bcd3 2500  ; WATER_STONE
+	bcd3 5000  ; FIRE_STONE
+	bcd3 5000  ; THUNDER_STONE
+	bcd3 5000  ; WATER_STONE
 	bcd3 10000 ; HP_UP
 	bcd3 10000 ; PROTEIN
 	bcd3 10000 ; IRON
@@ -46,25 +46,25 @@ ItemPrices::
 	bcd3 0     ; XXX
 	bcd3 0     ; BIKE_VOUCHER
 	bcd3 100   ; X_ACCURACY
-	bcd3 2500  ; LEAF_STONE
+	bcd3 5000  ; LEAF_STONE
 	bcd3 0     ; CARD_KEY
 	bcd3 48000 ; NUGGET
-	bcd3 10000 ; XXX PP_UP
+	bcd3 0     ; XXX PP_UP
 	bcd3 100   ; POKE_DOLL
 	bcd3 1000  ; FULL_HEAL
 	bcd3 2000  ; REVIVE
 	bcd3 6000  ; MAX_REVIVE
 	bcd3 100   ; GUARD_SPEC
-	bcd3 250   ; SUPER_REPEL
-	bcd3 500   ; MAX_REPEL
+	bcd3 500   ; SUPER_REPEL
+	bcd3 1000  ; MAX_REPEL
 	bcd3 100   ; DIRE_HIT
 	bcd3 10    ; COIN
-	bcd3 100   ; FRESH_WATER
-	bcd3 200   ; SODA_POP
-	bcd3 300   ; LEMONADE
+	bcd3 50    ; FRESH_WATER
+	bcd3 100   ; SODA_POP
+	bcd3 150   ; LEMONADE
 	bcd3 0     ; S_S_TICKET
 	bcd3 0     ; GOLD_TEETH
-	bcd3 2500  ; LINKING_CORD
+	bcd3 5000  ; LINKING_CORD
 	bcd3 100   ; X_DEFEND
 	bcd3 100   ; X_SPEED
 	bcd3 100   ; X_SPECIAL
@@ -80,9 +80,9 @@ ItemPrices::
 	bcd3 0     ; SUPER_ROD
 	bcd3 10000 ; PP_UP
 	bcd3 500   ; ETHER
-	bcd3 1500  ; MAX_ETHER
-	bcd3 1500  ; ELIXER
-	bcd3 3000  ; MAX_ELIXER
+	bcd3 1000  ; MAX_ETHER
+	bcd3 2000  ; ELIXER
+	bcd3 4000  ; MAX_ELIXER
 	assert_table_length NUM_ITEMS
 	bcd3 0     ; FLOOR_B2F
 	bcd3 0     ; FLOOR_B1F
