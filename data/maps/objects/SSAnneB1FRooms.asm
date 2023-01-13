65856dd06cb266cba88151ee73c093c15b36cd75
Arthur Ferreira <arthur_f@hotmail.com>
2023-01-13 15:21:16 -0500

    Update SSAnneB1FRooms.asm

--- data/maps/objects/SSAnneB1FRooms.asm
+++ data/maps/objects/SSAnneB1FRooms.asm
@@ -24,8 +24,8 @@ SSAnneB1FRooms_Object:
 	object_event  0,  4, SPRITE_FISHER, STAY, RIGHT, 6, OPP_FISHER, 2
 	object_event 10, 13, SPRITE_SUPER_NERD, STAY, RIGHT, 7 ; person
 	object_event 11, 12, SPRITE_MONSTER, STAY, NONE, 8 ; person
-	object_event 20,  2, SPRITE_POKE_BALL, STAY, NONE, 9, ETHER
+	object_event 20,  2, SPRITE_POKE_BALL, STAY, NONE, 9, FULL_RESTORE
 	object_event 10,  2, SPRITE_POKE_BALL, STAY, NONE, 10, TM_REST
-	object_event 12, 11, SPRITE_POKE_BALL, STAY, NONE, 11, MAX_POTION
+	object_event 12, 11, SPRITE_POKE_BALL, STAY, NONE, 11, RARE_CANDY
 
 	def_warps_to SS_ANNE_B1F_ROOMS
