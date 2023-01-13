1a67be69374fdf4a1b3fcdeb1eba6c61638d9447
Arthur Ferreira <arthur_f@hotmail.com>
2023-01-13 17:32:07 -0500

    Update arbok.asm

--- data/pokemon/base_stats/arbok.asm
+++ data/pokemon/base_stats/arbok.asm
@@ -10,7 +10,7 @@
 	INCBIN "gfx/pokemon/front/arbok.pic", 0, 1 ; sprite dimensions
 	dw ArbokPicFront, ArbokPicBack
 
-	db WRAP, SCREECH, ACID, BITE ; level 1 learnset
+	db WRAP, GLARE, ACID, BITE ; level 1 learnset
 	db GROWTH_MEDIUM_FAST ; growth rate
 
 	; tm/hm learnset
