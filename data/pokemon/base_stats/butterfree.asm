8602d953889c0a4530f5b159479375ccc874c354
Arthur Ferreira <arthur_f@hotmail.com>
2023-01-13 19:34:59 -0500

    Update butterfree.asm

--- data/pokemon/base_stats/butterfree.asm
+++ data/pokemon/base_stats/butterfree.asm
@@ -1,6 +1,6 @@
 	db DEX_BUTTERFREE ; pokedex id
 
-	db  60,  45,  50,  70,  80
+	db  60,  45,  50,  70, 105
 	;   hp  atk  def  spd  spc
 
 	db BUG, FLYING ; type
@@ -10,7 +10,7 @@
 	INCBIN "gfx/pokemon/front/butterfree.pic", 0, 1 ; sprite dimensions
 	dw ButterfreePicFront, ButterfreePicBack
 
-	db CONFUSION, NO_MOVE, NO_MOVE, NO_MOVE ; level 1 learnset
+	db GUST, HARDEN, STRING_SHOT, TACKLE ; level 1 learnset
 	db GROWTH_MEDIUM_FAST ; growth rate
 
 	; tm/hm learnset
