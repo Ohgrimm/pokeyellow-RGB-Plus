799d6d23fb20f8d19af833c3c03645d89c43dcb3
Arthur Ferreira <arthur_f@hotmail.com>
2023-01-13 19:54:21 -0500

    Update charmander.asm

--- data/pokemon/base_stats/charmander.asm
+++ data/pokemon/base_stats/charmander.asm
@@ -1,6 +1,6 @@
 	db DEX_CHARMANDER ; pokedex id
 
-	db  39,  52,  43,  65,  50
+	db  40,  55,  45,  65,  70
 	;   hp  atk  def  spd  spc
 
 	db FIRE, FIRE ; type
@@ -16,7 +16,7 @@
 	; tm/hm learnset
 	tmhm MEGA_PUNCH,   SWORDS_DANCE, MEGA_KICK,    TOXIC,        BODY_SLAM,    \
 	     TAKE_DOWN,    DOUBLE_EDGE,  SUBMISSION,   COUNTER,      SEISMIC_TOSS, \
-	     RAGE,         DRAGON_RAGE,  DIG,          MIMIC,        DOUBLE_TEAM,  \
+	     RAGE,         DRAGONBREATH,  DIG,          MIMIC,        DOUBLE_TEAM,  \
 	     REFLECT,      BIDE,         FIRE_BLAST,   SWIFT,        SKULL_BASH,   \
 	     REST,         SUBSTITUTE,   CUT,          STRENGTH
 	; end
